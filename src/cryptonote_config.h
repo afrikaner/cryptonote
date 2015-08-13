ac67f74ab294131faf274990c7c485ce65f5d236
afrikaner <boereafrikaner1838@gmail.com>
2015-08-13 23:12:06 +0200

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -101,8 +101,8 @@ const unsigned THREAD_STACK_SIZE                             = 5 * 1024 * 1024;
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"your_seed_ip1.com:8080",
-  //"your_seed_ip2.com:8080",
+  "196.38.48.109:17236",
+  "196.38.48.142:17236",
 };
 
 struct CheckpointData {
